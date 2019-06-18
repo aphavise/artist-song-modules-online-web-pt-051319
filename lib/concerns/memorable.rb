7283547c3b7e37f114d1badf60a65e61255f97a1
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 02:57:05 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,18 +2,20 @@ require_relative '../config/environment'
 
 module Memorable 
   module ClassMethods
+    
+    def reset_all
+      self.all.clear
+    end
   
-  def reset_all
-    self.all.clear
+    def count
+      self.all.count
+    end
   end
-
-  def count
-    self.all.count
-  end
-  
+    
   # module InstanceMethods
   #   def initialize
   #     self.class.all << self
   # end
-  end
+
+end
   
