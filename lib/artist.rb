4cd100ba03a0d736044fe5291666ab8d19ff8013
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 03:38:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,9 +7,6 @@ class Artist
   
   extend Memorable::ClassMethods
   
-
-
-
   @@artists = []
 
   def initialize
@@ -19,7 +16,7 @@ class Artist
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
-  end
+  # end
 
   def self.all
     @@artists
