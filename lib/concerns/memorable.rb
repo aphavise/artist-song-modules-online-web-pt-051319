25f875578d5c9b7ae884fd117d1f3c03280bef1e
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 02:35:54 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,13 @@
 module Memorable 
-  def ClassMethods
+  module ClassMethods
   
-  def self.reset_all
+  def reset_all
     self.all.clear
   end
 
-  def self.count
+  def count
     self.all.count
   end
-  
   
 end 
 
